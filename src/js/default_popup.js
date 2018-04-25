db831997df36029fe9883e68bb92cf8e339406d9
ruchenshanghai <996956441@qq.com>
2018-04-25 11:06:21 +0800

    learn Chrome extension: echart load ok, need to make sure element-ui and vue component ok

--- src/js/default_popup.js
+++ src/js/default_popup.js
@@ -10,7 +10,7 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
-const app = new Vue({
+const rootApp = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
@@ -19,3 +19,26 @@ const app = new Vue({
     console.log('loaded');
   }
 });
+
+var myChart = echarts.init(document.getElementById('main'));
+
+// 指定图表的配置项和数据
+var option = {
+  title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  legend: {
+    data:['销量']
+  },
+  xAxis: {
+    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]
+};
+myChart.setOption(option);
